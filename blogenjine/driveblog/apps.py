f177ea9a782e484f83dbda06486e1b1767428bb4
Yura Nikitin <vasilek199888@yandex.ru>
2020-01-06 23:21:45 +0300

    first commit/add Posts/routing/render Html

---
+++ blogenjine/driveblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DriveblogConfig(AppConfig):
+    name = 'driveblog'
---
+++ blogenjine/driveblog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+
+
+# Create your views here.
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'driveblog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'driveblog/post_detail.html', context={'post': post})
